a8e81209f92ae1e682ac3a53aa16ddbf05775132
Sang-minKIM <kimpran@naver.com>
2024-01-15 22:33:33 +0900

    chore: delete build file

--- output/JS/controllers/init/initUploadModal.js
+++
@@ -1,9 +0,0 @@
-import { UploadView } from "../../views/pages/UploadView.js";
-
-const modal = document.querySelector(".modal");
-
-export const initUploadModal = () => {
-  const modalHTML = UploadView();
-  modal.innerHTML = modalHTML;
-  initEventListener();
-};
