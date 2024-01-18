59f0c747d6126e087147ec08b26d4d12a1cad97b
Sang-minKIM <kimpran@naver.com>
2024-01-18 15:21:12 +0900

    fix: remove modal click fn

--- src/ts/controllers/events/onClick.ts
+++ src/ts/controllers/events/onClick.ts
@@ -1,4 +1,3 @@
-import { textCountHandler } from "../handlers/textCountHandler.ts";
 import { gotoNearWagle } from "../handlers/nearWagleHandler.ts";
 import { likePostHandler } from "../handlers/likePostHandler.ts";
 import {
@@ -8,9 +7,12 @@ import {
 } from "../handlers/uploadCardHandler.ts";
 import { closeDialog } from "../handlers/modalHandler.ts";
 
-const clickHandlerMap = {
+interface ClickHandlerMap {
+  [key: string]: (target: HTMLElement) => void;
+}
+
+const clickHandlerMap: ClickHandlerMap = {
   "js-onUpload": uploadCardHandler,
-  "js-textCount": textCountHandler,
   "js-onTagSelect": tagSelectHandler,
   "js-onImgSelect": imgSelectHandler,
   "js-nearWagle": gotoNearWagle,
@@ -18,19 +20,11 @@ const clickHandlerMap = {
   "js-onCloseModal": closeDialog,
 };
 
-export const onClick = (event) => {
-  const { target } = event;
-  const executeHandler = clickHandlerMap[target.classList[0]];
-  if (executeHandler) {
-    executeHandler(target);
-  }
-};
-
-export const onModalClick = (event) => {
-  const { target } = event;
-
-  const executeHandler = clickHandlerMap[target.classList[0]];
-  if (executeHandler) {
-    executeHandler(target);
+export const onClick = ({ target }: MouseEvent) => {
+  if (target instanceof HTMLElement) {
+    const executeHandler = clickHandlerMap[target.classList[0]];
+    if (executeHandler) {
+      executeHandler(target);
+    }
   }
 };
