9818970a3e1b34c2c0d8ac247a1111c493f2c50a
Sang-minKIM <kimpran@naver.com>
2024-01-18 15:18:03 +0900

    refactor: add fn type

--- src/ts/controllers/utils/getStationId.ts
+++ src/ts/controllers/utils/getStationId.ts
@@ -1,4 +1,4 @@
-export const getStationId = () => {
+export const getStationId = (): string => {
   const { pathname } = window.location;
   const stationId = pathname.split("/")[2];
   return stationId;
