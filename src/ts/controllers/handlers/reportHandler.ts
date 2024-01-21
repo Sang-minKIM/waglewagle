26430f8196a1c557ed6d42e6e6ec53dc90c2baa7
Sang-minKIM <kimpran@naver.com>
2024-01-21 15:58:11 +0900

    chore: remove annotation

--- src/ts/controllers/handlers/reportHandler.ts
+++ src/ts/controllers/handlers/reportHandler.ts
@@ -4,15 +4,6 @@ import { renderReportModal } from "../render";
 const report = document.querySelector(".report") as HTMLDialogElement;
 const confirm = document.querySelector(".confirm") as HTMLDialogElement;
 
-// // 신고 버튼을 클릭하면 신고 요청
-// dialog.querySelector(".report").addEventListener("click", () => {
-//   const postId = 77; // 신고하는 게시글의 ID
-//   const reportId = 1; // 신고 사유의 ID
-
-//   // 신고 요청 보내기
-//   report(postId, reportId);
-// });
-
 const showConfirm = () => {
   confirm.showModal();
 };
