9534291c5062cdc9f1e6850e21010c14c89df8a2
Sang-minKIM <kimpran@naver.com>
2024-01-21 16:57:17 +0900

    feat: make bad post hidden

--- src/ts/controllers/handlers/reportHandler.ts
+++ src/ts/controllers/handlers/reportHandler.ts
@@ -1,5 +1,7 @@
+import { fetchCardList } from "../api/getApi";
 import { postReport } from "../api/postApi";
 import { renderReportModal } from "../render";
+import { getStationId } from "../utils/getStationId";
 import { saveReport } from "../utils/reportFn";
 
 const report = document.querySelector(".report") as HTMLDialogElement;
@@ -23,7 +25,8 @@ export const closeReport = () => {
 
 export const confirmHandler = () => {
   closeConfirm();
-  // 리렌더링
+  const stationId = getStationId();
+  fetchCardList({ stationId });
 };
 
 const DB_INDEX_CORRECTION_FACTOR = 1;
