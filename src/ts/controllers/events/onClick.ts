f7b2d7736ee4e1dd3ff57b67cf90315acf8ad345
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2024-03-23 16:53:00 +0900

    refactor: use optional chaining instead of if condition

--- src/ts/controllers/events/onClick.ts
+++ src/ts/controllers/events/onClick.ts
@@ -1,40 +1,29 @@
 import { gotoNearWagle } from "../handlers/nearWagleHandler.ts";
 import { likePostHandler } from "../handlers/likePostHandler.ts";
-import {
-  uploadCardHandler,
-  tagSelectHandler,
-  imgSelectHandler,
-} from "../handlers/uploadCardHandler.ts";
+import { uploadCardHandler, tagSelectHandler, imgSelectHandler } from "../handlers/uploadCardHandler.ts";
 import { closeDialog } from "../handlers/modalHandler.ts";
-import {
-  openReport,
-  closeReport,
-  submitReport,
-  confirmHandler,
-} from "../handlers/reportHandler.ts";
+import { openReport, closeReport, submitReport, confirmHandler } from "../handlers/reportHandler.ts";
 
 interface ClickHandlerMap {
-  [key: string]: (target: HTMLElement) => void;
+    [key: string]: (target: HTMLElement) => void;
 }
 
 const clickHandlerMap: ClickHandlerMap = {
-  "js-onUpload": uploadCardHandler,
-  "js-onTagSelect": tagSelectHandler,
-  "js-onImgSelect": imgSelectHandler,
-  "js-nearWagle": gotoNearWagle,
-  "js-likePost": likePostHandler,
-  "js-onCloseModal": closeDialog,
-  "js-onOpenReport": openReport,
-  "js-onCloseReport": closeReport,
-  "js-onSubmitReport": submitReport,
-  "js-onConfirm": confirmHandler,
+    "js-onUpload": uploadCardHandler,
+    "js-onTagSelect": tagSelectHandler,
+    "js-onImgSelect": imgSelectHandler,
+    "js-nearWagle": gotoNearWagle,
+    "js-likePost": likePostHandler,
+    "js-onCloseModal": closeDialog,
+    "js-onOpenReport": openReport,
+    "js-onCloseReport": closeReport,
+    "js-onSubmitReport": submitReport,
+    "js-onConfirm": confirmHandler,
 };
 
 export const onClick = ({ target }: MouseEvent) => {
-  if (target instanceof HTMLElement) {
-    const executeHandler = clickHandlerMap[target.classList[0]];
-    if (executeHandler) {
-      executeHandler(target);
+    if (target instanceof HTMLElement) {
+        const executeHandler = clickHandlerMap[target.classList[0]];
+        executeHandler?.(target);
     }
-  }
 };
